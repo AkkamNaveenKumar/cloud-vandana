47ecd774169e388033253341deba0d4f0743b8f7
AkkamNaveenKumar <142741026+AkkamNaveenKumar@users.noreply.github.com>
2023-10-19 00:56:03 +0530

    Add files via upload

---
+++ JAVA/Panagram.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Panagram {
+	
+	static boolean isPanagram(String s) {
+		String t = "abcdefghijklmnopqrstuvwxyz";
+		s = s.toLowerCase();
+		int count = 0;
+		for(int i = 0; i < t.length(); i++) {
+			if(s.indexOf(t.charAt(i)) >= 0) {
+				count++;
+			}
+			else {
+				break;
+			}	
+		}
+		if(count == 26) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		System.out.println(isPanagram(s));
+        
+
+	}
+
+}
